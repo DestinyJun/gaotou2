88394aa618144bf7ea1f93888b0eb6527b3120ad
文君 <429771890@qq.com>
2018-12-18 09:39:59 +0800

    接口对接： （1）省市人流接口对接完毕，打包

--- src/app/common/services/global.service.ts
+++ src/app/common/services/global.service.ts
@@ -5,13 +5,16 @@ import {LocalStorageService} from './local-storage.service';
 @Injectable()
 export class GlobalService {
   public accessToken: string;
-  public urls = `http://120.78.137.182:8888/highway-management`;
+  // gaotou
+ /* public urls = `http://120.78.137.182:8888/highway-management`;
   public urlc = `http://120.78.137.182:8888/highway-interactive`;
-  public urla = `http://120.77.171.73:8080/highway-authentication`;
-  public urlt = `http://123.249.28.108:8889/highway-interactive`;
-  /*public urls = `http://123.249.28.108:8082/highway-management`;
-  public urlc = `http://123.249.28.108:8082/highway-interactive`;
   public urla = `http://120.77.171.73:8080/highway-authentication`;*/
+  // gaotou3
+  public urls = `http://123.249.28.108:8082/highway-management`;
+  public urlc = `http://123.249.28.108:8082/highway-interactive`;
+  public urla = `http://120.77.171.73:8080/highway-authentication`;
+  // 客流
+  public urlt = `http://123.249.28.108:8889/highway-interactive`;
   constructor(
     private http: HttpClient,
     private localSessionStorage: LocalStorageService
