d546578f2035a70ab31e8e4c29a4432b7ec0c45d
文君 <429771890@qq.com>
2019-02-19 16:56:44 +0800

    操作日志： （1）整了一堆配置，浪费时间

--- src/environments/environment.gaotou.ts
+++ src/environments/environment.gaotou.ts
@@ -4,8 +4,8 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
-  env: '开发——真实数据',
+  production: true,
+  env: 'gaotou真实数据！！！',
   urls: 'http://120.78.137.182:8888/highway-management', // 管理端
   urlc: 'http://120.78.137.182:8888/highway-interactive', // 客户端
   urla: 'http://120.77.171.73:8080/highway-authentication', // 独立认证端
