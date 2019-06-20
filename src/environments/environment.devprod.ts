0a390755c33eea7536105dcaafb73c5fb9961b9f
DestinyJun <429771890@qq.com>
2019-06-20 20:45:44 +0800

    操作日志： （1）
    ————————生活需要方向和激情，希望你明白——————

--- src/environments/environment.devprod.ts
+++ src/environments/environment.devprod.ts
@@ -6,7 +6,7 @@
 export const environment = {
   production: false,
   env: '开发——真实数据',
-  urls: 'http://120.78.137.182:8888/highway-management', // 管理端
-  urlc: 'http://120.78.137.182:8888/highway-interactive', // 客户端
+  urls: 'http://120.77.171.73:8888/highway-management', // 管理端
+  urlc: 'http://120.77.171.73:8888/highway-interactive', // 客户端
   urla: 'http://120.77.171.73:8080/highway-authentication', // 独立认证端
 };
