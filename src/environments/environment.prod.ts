5d8ef4b09ab0fc60dd21ad63893e0e059c53e35a
DestinyJun <429771890@qq.com>
2019-08-08 11:39:24 +0800

    操作日志： （1）修改部分pakejson
    ————————生活需要方向和激情，希望你明白——————

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  env: '欢迎您来到我们的产品网站！！！',
+  env: '欢迎来到我们的系统.....',
   urls: 'http://120.77.171.73:8888/highway-management', // 管理端
   urlc: 'http://139.9.153.27:8080/highway-interactive', // 客户端
   urla: 'http://139.9.225.98:8081/highway-authentication', // 独立认证端
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  env: '开发——测试数据',
+  env: '开发中......',
   urls: 'http://120.77.171.73:8888/highway-management', // 管理端
   urlc: 'http://139.9.153.27:8080/highway-interactive', // 客户端
   urla: 'http://139.9.225.98:8081/highway-authentication', // 独立认证端
