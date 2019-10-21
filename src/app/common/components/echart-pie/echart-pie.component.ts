27621233dc96010a1f4c52de85dd475bdc9728dc
DestinyJun <429771890@qq.com>
2019-10-21 11:10:04 +0800

    操作日志： （1）修改某些属性
    ————————生活需要方向和激情，希望你明白——————

--- src/app/common/components/echart-pie/echart-pie.component.ts
+++ src/app/common/components/echart-pie/echart-pie.component.ts
@@ -15,6 +15,7 @@ export class EchartPieComponent implements OnInit, OnChanges {
   ngOnInit() {}
   ngOnChanges(changes: SimpleChanges): void {
     if (this.option) {
+      console.log(this.option);
       this.optionsPie();
     }
   }
