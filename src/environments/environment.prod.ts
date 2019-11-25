09d0899411b9fd6d1b4168364194a6730598b4af
DestinyJun <429771890@qq.com>
2019-11-25 11:18:53 +0800

    操作日志： （1）切换接口地址
    ————————生活需要方向和激情，希望你明白——————

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   env: '欢迎您来到我们的产品网站！！！',
-  urls: 'http://120.78.137.182:8888/highway-management', // 管理端
-  urlc: 'http://120.78.137.182:8888/highway-interactive', // 客户端
-  urla: 'http://120.77.171.73:8080/highway-authentication', // 独立认证端
+  urls: 'http://139.9.155.62:8080/highway-management', // 管理端
+  urlc: 'http://139.9.153.27:8080/highway-interactive', // 客户端
+  urla: 'http://139.9.225.98:8081/highway-authentication', // 独立认证端
 };
